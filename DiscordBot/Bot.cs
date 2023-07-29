bac6f0befefb31f224f99699c652d3e1287315ec
Shawtygold <shawtygoldz@gmail.com>
2023-07-29 12:49:03 +0500

    Добавьте файлы проекта.

---
+++ DiscordBot/Bot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    internal class Bot
+    {
+
+
+
+    }
+}
