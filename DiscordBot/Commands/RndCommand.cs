6fd43e1a0fe94a4c491a43c96aae82c3977318e8
Shawtygold <shawtygoldz@gmail.com>
2023-08-07 12:09:41 +0500

    Добавил реализацию команды Random

--- DiscordBot/Commands/RndCommand.cs
+++ DiscordBot/Commands/RndCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace DiscordBot.Commands
 {
-    internal class RndCommand
+    internal class RndCommand : BaseCommandModule
     {
+
+        [Command("random")]
+        public async Task RandomCommand(CommandContext ctx, int from, int to)
+        {
+            var random = new Random();
+            await ctx.Channel.SendMessageAsync($"Рандомное число: {random.Next(from, to)}");
+        }
     }
 }
